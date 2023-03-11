927c1a40d2b8ea5b14c632bcfcbeff02b21621e0
Nuno Silva <59452877+komen205@users.noreply.github.com>
2023-03-11 00:08:33 +0000

    Fix possible application as a simple webview

---
+++ app/src/main/java/io/github/scafer/prices/crawler/mobile/MainActivity.java
@@ -0,0 +1,32 @@
+package io.github.scafer.prices.crawler.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hides top bar
+        getSupportActionBar().hide();
+
+        WebView webview = findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        //Enables javascript
+        webSettings.setJavaScriptEnabled(true);
+        //Will enable local storage
+        webSettings.setDomStorageEnabled(true);
+
+        webview.loadUrl(BuildConfig.SERVER_URL);
+    }
+}
